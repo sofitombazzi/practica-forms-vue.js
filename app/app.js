5758487f0885844e1c42493d78caa9fc9aafae63
Sofia Tombazzi <128006502+sofitombazzi@users.noreply.github.com>
2024-05-02 05:48:57 -0300

    feat: cree mis archivos base de vue

---
+++ app/app.js
@@ -0,0 +1,14 @@
+// Creo mi instancia principal
+const app = Vue.createApp({
+    data(){
+        return{
+            // Acá va mi contenido estático
+        }
+    }
+});
+
+// Aca van mis componentes, templates, methods
+
+
+// Acá monto la instancia 
+app.mount('.contenedor');
