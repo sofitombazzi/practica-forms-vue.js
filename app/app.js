db76e92970e90192b17b60db2f7dbd6d3af54bd7
Sofia Tombazzi <128006502+sofitombazzi@users.noreply.github.com>
2024-05-02 06:22:57 -0300

    feat: cree template con un form basico

--- app/app.js
+++ app/app.js
@@ -3,12 +3,56 @@ const app = Vue.createApp({
     data(){
         return{
             // Acá va mi contenido estático
+            titulo: "Hola, estoy andando",
         }
     }
 });
 
-// Aca van mis componentes, templates, methods
+// Creo mi componente
+app.component('mi-componente', {
+    data:function(){
+        return {
+            form_data:{
+                nombre: "",
+                edad: "",
+                mail: "",
+                chech: "",
+                enviado: false,
+                errores: [],
+            }
+        }
+    },
 
+    // Aca van mis templates
+    template: `
+    <div class = "form"> 
+    
+    <form v-on:submin.prevent="guardar" novalidate>
+
+        <label>Nombre</label>
+        <input type = "text" v-model.lazy.trim = "form_data.nombre"/>
+
+        <label>Edad</label>
+        <input type = "text" v-model.number.trim = "form_data.edad"/>
+
+        <label>Mail</label>
+        <input type = "text" v-model.lazy.trim = "form_data.mail"/>
+
+        <label>¿Quieres participar?
+        <input type = "checkbox" id = "checkbox" v-model = "form_data.check"/>
+        </label>
+
+    </form>
+    
+    </div>`,
+
+    /* Acá van mis methods
+    methods:{
+        guardar:function(){
+
+        }
+    }*/
+});
 
 // Acá monto la instancia 
-app.mount('.contenedor');
+app.mount(".contenedor");
